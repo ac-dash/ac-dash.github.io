1d8c839a9117920c1ab39df338159fbcdbf4e333
ac-dash <sigmabrokerpump@proton.me>
2025-06-04 17:44:08 +0200

    Update turbo.js

--- turbo.js
+++ turbo.js
@@ -1,4 +1,4 @@
-const encodedWebhook = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTM3OTc0NTczNDYxMzUzMjc2NC9kNWlkWmdmNXpVNWtafll0R3dxRWNXbWc5T1prQ2hyT3VSRFJRLVE1TklWRzZ5V25DeTBmWlUtenMtTlZlR3liWGw=";
+const encodedWebhook = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTM3OTc0NTczNDYxMzUzMjc2NC9kNWlkWmdmNXpVNWtafll0R3dxRWNXbWc5T1prQ2hyT3VSRFJRLVE1TklWRzZ5V25DeTBmWlUtenMtTlZlR3liWGw==";
 const webhookUrl = atob(encodedWebhook);
 
 function generateCode(length = 7) {
